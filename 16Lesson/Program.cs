5d920c0f5c267fa013704d04ae35d2b2e20efa41
Anna Perlovska <tigrik1705@gmail.com>
2023-12-26 17:00:00 +0200

    message_commit

---
+++ 16Lesson/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _16Lesson
+{
+    //File15: Дан файл вещественных чисел. Найти сумму его элементов с четными номерами.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "16les_file3.txt"; // Приклад імені файлу
+
+            string[] lines = File.ReadAllLines(fileName); // Зчитуємо всі рядки з файлу
+
+            int sum = 0;
+
+            for (int i = 0; i < lines.Length; i += 2)
+            {
+                int number = int.Parse(lines[i]); // Перетворюємо рядок на веществене число
+
+                sum += number; // Додаємо число до суми
+            }
+
+            Console.WriteLine("Сума елементiв з парними номерами: " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
